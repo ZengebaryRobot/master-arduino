2cbf9b5657c054ee57f9fea9a82271a4f178e653
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2025-04-30 18:08:33 +0300

    Update Arm.h

--- Arm.h
+++ Arm.h
@@ -2,8 +2,7 @@
 #include <NeoSWSerial.h>
 #include "DEFs.h"
 
-enum ArmMotor
-{
+enum ArmMotor {
   BASE = 0,
   SHOULDER = 1,
   ELBOW = 2,
@@ -11,8 +10,7 @@ enum ArmMotor
   GRIP = 4
 };
 
-class Arm
-{
+class Arm {
 private:
   NeoSWSerial *debugSerial;
   Servo servoBase, servoShoulder, servoElbow, servoWrist, servoGrip;
@@ -20,22 +18,17 @@ private:
   // Current angles
   int state_angle_base, state_angle_shoulder, state_angle_elbow, state_angle_wrist, state_angle_grip;
 
-  void printMessage(const char *msg, bool newLine = false)
-  {
+  void printMessage(const char *msg, bool newLine = false) {
 #if ENABLE_DEBUG
-    if (newLine)
-    {
+    if (newLine) {
       debugSerial->println(msg);
-    }
-    else
-    {
+    } else {
       debugSerial->print(msg);
     }
 #endif
   }
 
-  void printMenu()
-  {
+  void printMenu() {
 #if ENABLE_DEBUG
     debugSerial->print("B: ");
     debugSerial->println(state_angle_base);
@@ -50,37 +43,38 @@ private:
 #endif
   }
 
-  void attachAll()
-  {
+  void attachAll() {
     servoBase.attach(SERVO_PIN_BASE);
     servoShoulder.attach(SERVO_PIN_SHOULDER);
     servoElbow.attach(SERVO_PIN_ELBOW);
     servoWrist.attach(SERVO_PIN_WRIST, 500, 2400);
-    servoGrip.attach(SERVO_PIN_GRIP, 500, 2400); // 1000 - 2000 ?? REVIEW
+    servoGrip.attach(SERVO_PIN_GRIP, 500, 2400);  // 1000 - 2000 ?? REVIEW
   }
 public:
-  Arm(NeoSWSerial *serial)
-  {
+  Arm(NeoSWSerial *serial) {
+    debugSerial = serial;
+  }
+
+  void initializeArm() {
+
     state_angle_base = DEFAULT_ANGLE_BASE;
     state_angle_shoulder = DEFAULT_ANGLE_SHOULDER;
     state_angle_elbow = DEFAULT_ANGLE_ELBOW;
     state_angle_wrist = DEFAULT_ANGLE_WRIST;
     state_angle_grip = DEFAULT_ANGLE_GRIP;
-
-    debugSerial = serial;
-  }
-
-  void initializeArm()
-  {
     attachAll();
+    moveServo(BASE, DEFAULT_ANGLE_BASE, 0);
+    moveServo(SHOULDER, DEFAULT_ANGLE_SHOULDER, 4);
+    moveServo(ELBOW, DEFAULT_ANGLE_ELBOW, 0);
+    moveServo(WRIST, DEFAULT_ANGLE_WRIST, 4);
+    moveServo(GRIP, DEFAULT_ANGLE_GRIP, 0);
 
     printMessage("Arm Motors intialized", true);
     printMenu();
     delay(500);
   }
 
-  void moveServo(ArmMotor motor, int requiredAngle, int overShoot)
-  {
+  void moveServo(ArmMotor motor, int requiredAngle, int overShoot) {
     // Clamp angle to safe range
     requiredAngle = constrain(requiredAngle, 10, 170);
 
@@ -90,70 +84,67 @@ public:
     Servo *s = nullptr;
     int *stateAngle = nullptr;
 
-    switch (motor)
-    {
-    case BASE:
-      threshold = 20;
-      motorName = "base";
-      s = &servoBase;
-      stateAngle = &state_angle_base;
-      break;
-    case SHOULDER:
-      threshold = 0;
-      motorName = "shoulder";
-      s = &servoShoulder;
-      stateAngle = &state_angle_shoulder;
-      break;
-    case ELBOW:
-      threshold = 5;
-      motorName = "elbow";
-      s = &servoElbow;
-      stateAngle = &state_angle_elbow;
-      break;
-    case WRIST:
-      threshold = 0;
-      motorName = "wrist";
-      s = &servoWrist;
-      stateAngle = &state_angle_wrist;
-      break;
-    case GRIP:
-      threshold = 0;
-      motorName = "grip";
-      s = &servoGrip;
-      stateAngle = &state_angle_grip;
-      break;
-    default:
-      return;
+    switch (motor) {
+      case BASE:
+        threshold = 20;
+        motorName = "base";
+        s = &servoBase;
+        stateAngle = &state_angle_base;
+        break;
+      case SHOULDER:
+        threshold = 0;
+        motorName = "shoulder";
+        s = &servoShoulder;
+        stateAngle = &state_angle_shoulder;
+        break;
+      case ELBOW:
+        threshold = 5;
+        motorName = "elbow";
+        s = &servoElbow;
+        stateAngle = &state_angle_elbow;
+        break;
+      case WRIST:
+        threshold = 0;
+        motorName = "wrist";
+        s = &servoWrist;
+        stateAngle = &state_angle_wrist;
+        break;
+      case GRIP:
+        threshold = 0;
+        motorName = "grip";
+        s = &servoGrip;
+        stateAngle = &state_angle_grip;
+        break;
+      default:
+        return;
     }
 
-    if (requiredAngle == *stateAngle)
-    {
-      printMessage(motorName);
-      printMessage(" is already on angle: ");
-      printMessage(requiredAngle, true);
-      return;
-    }
+    // if (requiredAngle == *stateAngle) {
+    //   printMessage(motorName);
+    //   printMessage(" is already on angle: ");
+    //   printMessage(requiredAngle, true);
+    //   return;
+    // }
 
     // Move to target angle
     int step = (requiredAngle > *stateAngle) ? 1 : -1;
-    for (int i = *stateAngle; i != requiredAngle + step; i += step)
-    {
+    for (int i = *stateAngle; i != requiredAngle + step; i += step) {
       int remaining = abs(requiredAngle - i);
       int delayMs = (remaining > threshold) ? baseDelay : slowDelay;
       s->write(i);
+#if ENABLE_DEBUG
+      debugSerial->println(i);
+#endif
       delay(delayMs);
     }
 
     // Applying overshoot
-    if (overShoot > 0)
-    {
-      for (int i = requiredAngle; i <= requiredAngle + overShoot; i++)
-      {
+    if (overShoot > 0) {
+      for (int i = requiredAngle; i <= requiredAngle + overShoot; i++) {
         s->write(i);
         delay(baseDelay);
       }
-      for (int i = requiredAngle + overShoot; i >= requiredAngle; i--)
-      {
+      for (int i = requiredAngle + overShoot; i >= requiredAngle; i--) {
         s->write(i);
         delay(baseDelay);
       }
@@ -169,8 +160,7 @@ public:
     delay(500);
   }
 
-  void moveArmTo(int base, int shoulder, int elbow, int wrist, int grip)
-  {
+  void moveArmTo(int base, int shoulder, int elbow, int wrist, int grip) {
     // to be implemented
   }
 };
