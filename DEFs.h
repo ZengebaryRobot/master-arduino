d8d6c4b694aac090b957bef213b6d5357eda62e3
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2025-05-06 18:35:20 +0300

    Updated arm pins

--- DEFs.h
+++ DEFs.h
@@ -2,13 +2,13 @@
 #define ENABLE_DEBUG 0
 
 // === Servo Pins ===
-#define SERVO_PIN_BASE 6     // Base
+#define SERVO_PIN_BASE 10     // Base
 #define SERVO_PIN_SHOULDER 3 // Shoulder
 #define SERVO_PIN_ELBOW 9    // Elbow
-#define SERVO_PIN_WRIST 8    // Wrist
-#define SERVO_PIN_GRIP 7     // Grip
+#define SERVO_PIN_WRIST 6    // Wrist
+#define SERVO_PIN_GRIP 5     // Grip
 
-#define GRIP_CLOSED 72
+#define GRIP_CLOSED 80
 #define GRIP_OPEN 105
 
 // === Default Angles ===
