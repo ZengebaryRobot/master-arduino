1cad60c2c61514f1208984548ff91eb835d61c5c
AhmedSobhy01 <samahmedsobhy@gmail.com>
2025-05-11 02:17:21 +0300

    Add ESP_ENABLE pin configuration in setup

--- DEFs.h
+++ DEFs.h
@@ -1,8 +1,11 @@
 // === Debugging ===
 #define ENABLE_DEBUG 0
 
+// === Serial Ports ===
+#define ESP_ENABLE 11
+
 // === Servo Pins ===
-#define SERVO_PIN_BASE 10     // Base
+#define SERVO_PIN_BASE 10    // Base
 #define SERVO_PIN_SHOULDER 3 // Shoulder
 #define SERVO_PIN_ELBOW 9    // Elbow
 #define SERVO_PIN_WRIST 6    // Wrist
@@ -22,18 +25,18 @@
 #define STEPPER_COUNT 10
 
 #define STEP_PIN_1 A0
-#define DIR_PIN_1  A1
+#define DIR_PIN_1 A1
 
 #define STEP_PIN_2 A2
-#define DIR_PIN_2  A3
+#define DIR_PIN_2 A3
 
 #define STEP_PIN_3 A4
-#define DIR_PIN_3  A5
+#define DIR_PIN_3 A5
 
 #define STEP_PIN_4 12
-#define DIR_PIN_4  13
+#define DIR_PIN_4 13
 
 #define STEP_PIN_5 7
 #define DIR_PIN_5 8
 
-#define STEPPER_DELAY 1000  
+#define STEPPER_DELAY 1000
